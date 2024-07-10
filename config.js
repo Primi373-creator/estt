a2cb10f590129037ce836940e99ce5afa07986ac
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-07-10 04:43:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    PORT: 9078,
-    ADMIN:[''],
-    BASE_URL: 'https://consumet-api-gilt.vercel.app/',
-    TOKEN:'6581311248:AAExltZLFpcBq32p6nOLCYOJ_TQcTMP9sxU',
-    MONGO_URL: 'mongodb+srv://uploader2:uploader2@uploader2.uhnmx1u.mongodb.net/?retryWrites=true&w=majority&appName=uploader2', //your mongodb_url
-  }
+  PORT: 9078,
+  ADMIN: [""],
+  BASE_URL: "https://consumet-api-gilt.vercel.app/",
+  TOKEN: "7491364491:AAEG4sa9fpiXuWlCqwRBR2zQ1tAfCi-iZYo",
+  MONGO_URL:
+    "mongodb+srv://uploader2:uploader2@uploader2.uhnmx1u.mongodb.net/?retryWrites=true&w=majority&appName=uploader2", //your mongodb_url
+};
